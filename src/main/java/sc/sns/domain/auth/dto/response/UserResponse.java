c1dbc38bf20c46245883640807e641c4a80f6541
김승찬 <s24041@gsm.hs.kr>
2025-02-13 22:20:41 +0900

    feat: auth 관련 dto정의

---
+++ src/main/java/sc/sns/domain/auth/dto/response/UserResponse.java
@@ -0,0 +1,16 @@
+package sc.sns.domain.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+    private Long id;
+    private String email;
+}
+
