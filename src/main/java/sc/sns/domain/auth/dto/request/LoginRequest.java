0f5b1f14c19113748f037d7eedcabda0bcc54338
김승찬 <s24041@gsm.hs.kr>
2025-02-13 22:20:50 +0900

    feat: auth 관련 dto정의

---
+++ src/main/java/sc/sns/domain/auth/dto/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package sc.sns.domain.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+    private String email;
+    private String password;
+}
