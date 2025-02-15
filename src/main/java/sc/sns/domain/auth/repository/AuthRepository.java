2261cc88d674fefa431b38a8ef41a7ef2e60ba23
김승찬 <s24041@gsm.hs.kr>
2025-02-15 22:51:43 +0900

    feat: auth repository 추가

--- src/main/java/sc/sns/domain/auth/repository/AuthRepository.java
+++ src/main/java/sc/sns/domain/auth/repository/AuthRepository.java
@@ -1,7 +1,12 @@
 package sc.sns.domain.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import sc.sns.domain.auth.entity.RefreshToken;
 
-public interface AuthRepository extends JpaRepository<RefreshToken, String> {
+import java.util.Optional;
+
+public interface AuthRepository extends CrudRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+    void deleteByUserId(Long userId);
 }
