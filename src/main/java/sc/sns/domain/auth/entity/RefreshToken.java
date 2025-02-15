1509eb37e13b0c16702c2a803930a3dd9706237e
김승찬 <s24041@gsm.hs.kr>
2025-02-15 22:52:07 +0900

    feat: redis token entity 추가

--- src/main/java/sc/sns/domain/auth/entity/RefreshToken.java
+++ src/main/java/sc/sns/domain/auth/entity/RefreshToken.java
@@ -1,11 +1,11 @@
 package sc.sns.domain.auth.entity;
 
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.LocalDateTime;
 
