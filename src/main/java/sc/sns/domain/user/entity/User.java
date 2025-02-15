c7e1552010f8bcf36eba9121bd836a00757ed19a
김승찬 <s24041@gsm.hs.kr>
2025-02-15 22:51:21 +0900

    feat: User entity 추가

---
+++ src/main/java/sc/sns/domain/user/entity/User.java
@@ -0,0 +1,21 @@
+package sc.sns.domain.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
