053e22a212ab0a020fb5c2dbe061a52b421dd4c8
김승찬 <s24041@gsm.hs.kr>
2025-02-19 20:33:08 +0900

    feat: auth 관련 dto 추가

---
+++ src/main/java/sc/sns/domain/auth/presentation/dto/response/LoginResponse.java
@@ -0,0 +1,11 @@
+package sc.sns.domain.auth.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+    private String accessToken;
+    private String refreshToken;
+}
