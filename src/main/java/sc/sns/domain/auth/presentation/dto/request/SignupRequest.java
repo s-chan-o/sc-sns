e669d123fdeab08d083eede765d2fade065dbadf
김승찬 <s24041@gsm.hs.kr>
2025-02-19 20:33:49 +0900

    feat: auth 관련 dto 추가

---
+++ src/main/java/sc/sns/domain/auth/presentation/dto/request/SignupRequest.java
@@ -0,0 +1,19 @@
+package sc.sns.domain.auth.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest {
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+    private String password;
+
+    @NotBlank(message = "닉네임은 필수 입력값입니다.")
+    private String nickname;
+}
