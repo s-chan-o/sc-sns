0bc801c22cc0256ddb6e9b97e9bbf1f34d94a6af
김승찬 <s24041@gsm.hs.kr>
2025-02-19 20:33:11 +0900

    feat: auth 관련 dto 추가

---
+++ src/main/java/sc/sns/domain/auth/presentation/dto/request/LogoutRequest.java
@@ -0,0 +1,13 @@
+package sc.sns.domain.auth.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LogoutRequest {
+
+    @NotBlank(message = "리프레시 토큰은 필수 입력값입니다.")
+    private String refreshToken;
+}
