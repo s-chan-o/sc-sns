aa8299a4333e3d8fc0a2375a3167a743770cda84
김승찬 <s24041@gsm.hs.kr>
2025-02-20 19:45:29 +0900

    feat: errorresponse 추가

---
+++ src/main/java/sc/sns/global/exception/ErrorResponse.java
@@ -0,0 +1,11 @@
+package sc.sns.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+}
