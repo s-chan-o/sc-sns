c56b918b74b15baca7c6986f60abf3ae52ca4ad0
김승찬 <s24041@gsm.hs.kr>
2025-03-06 10:14:04 +0900

    style: dto 세부사항 변경

--- src/main/java/sc/sns/domain/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/sc/sns/domain/auth/presentation/dto/request/LoginRequest.java
@@ -1,16 +1,13 @@
 package sc.sns.domain.auth.presentation.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequest {
-
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
     private String email;
-
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 }
