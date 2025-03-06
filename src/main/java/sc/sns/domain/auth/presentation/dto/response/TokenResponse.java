f1d47b14938d21b592cb1fd533c4343a9c6e9602
김승찬 <s24041@gsm.hs.kr>
2025-03-06 10:15:35 +0900

    feat: token dto 정의

---
+++ src/main/java/sc/sns/domain/auth/presentation/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package sc.sns.domain.auth.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private Long expiresIn;
+}
